9f4891b3c0d59493b9958b9cf6178960b78bc6ad
Akira <akira.aida@uoit.net>
2018-01-10 15:28:16 -0500

    Added aliases for starting programs

--- config.fish
+++ config.fish
@@ -1,9 +1,16 @@
 function chrome
-    google-chrome > /dev/null 2>&1
+    google-chrome > /dev/null 2>&1 &
+    disown
 end
 
 function open
-    xdg-open $argv[1] > /dev/null 2>&1
+    xdg-open $argv[1] > /dev/null 2>&1 &
+    disown
+end
+
+function spot
+    spotify $argv[1] > /dev/null 2>&1 &
+    disown
 end
 
 function edfish
